934c8beedd1c2c17a2ede51f4f1bc687e90bcae6
gatoro741 <olegatorgator@gmail.com>
2025-01-21 14:38:57 +0500

    Exercise Access List Items

--- lab1.py
+++ lab1.py
@@ -1,3 +1,8 @@
-thislist = ["apple", "banana", "cherry"]
-if "apple" in thislist:
-  print("Yes, 'apple' is in the fruits list")
+mylist = ['apple', 'banana', 'cherry']
+print(mylist[-1])
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+mylist = ['apple', 'banana', 'cherry', 'orange', 'kiwi']
+print(mylist[1:4])
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
