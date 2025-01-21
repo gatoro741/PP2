24fc866fa08176db6987d6e760e78f0427c27ee6
gatoro741 <olegatorgator@gmail.com>
2025-01-21 13:35:40 +0500

    Python Assignment Operators

--- lab1.py
+++ lab1.py
@@ -1,7 +1,12 @@
-print(10 + 5)
-print(10 - 5)
-print(10 * 5)
-print(10 / 5)
-print(10 % 5)
-print(10 ** 5)
-print(10 // 5)
+x=5
+print(x)
+x+=5
+print(x)
+x-=5
+print(x)
+x*=5
+print(x)
+x/=5
+print(x)
+x%=5
+print(x)
