827f9a2a5de13462590134e39a6ed276eba18a64
gatoro741 <olegatorgator@gmail.com>
2025-01-21 13:46:26 +0500

    Python Lists

--- lab1.py
+++ lab1.py
@@ -1,12 +1,2 @@
-x = 5
-x += 3
-print(x)
-print(10 * 5)
-print(10 / 2)
-fruits = ["apple", "banana"]
-if "apple" in fruits:
-  print("Yes, apple is a fruit!")
-if 5 != 10:
-  print("5 and 10 is not equal")
-if 5 == 10 or 4 == 4:
-  print("At least one of the statements is true")
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
