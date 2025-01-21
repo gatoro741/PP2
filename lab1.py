25527f3828b8312218a8f97af0d81344e16600cb
gatoro741 <olegatorgator@gmail.com>
2025-01-21 14:30:37 +0500

    Access Items

--- lab1.py
+++ lab1.py
@@ -1,6 +1,2 @@
-mylist = ['apple', 'banana', 'cherry']
-print(mylist[1])
-mylist = ['apple', 'banana', 'banana', 'cherry']
-print(mylist[2])
-thislist = ['apple', 'banana', 'cherry']
-print(len(thislist))
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
