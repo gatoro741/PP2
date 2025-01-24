9c4f7948928cdfa47b527976de830349db145577
gatoro741 <olegatorgator@gmail.com>
2025-01-24 19:53:19 +0500

    change item value

--- lab1.py
+++ lab1.py
@@ -1,8 +1,3 @@
-mylist = ['apple', 'banana', 'cherry']
-print(mylist[-1])
-fruits = ["apple", "banana", "cherry"]
-print(fruits[1])
-mylist = ['apple', 'banana', 'cherry', 'orange', 'kiwi']
-print(mylist[1:4])
-fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(fruits[2:5])
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
