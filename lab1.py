acb247535f666fd99628d531bfb4711f76ef9260
gatoro741 <olegatorgator@gmail.com>
2025-01-24 20:07:26 +0500

    remove items

--- lab1.py
+++ lab1.py
@@ -1,8 +1,3 @@
-mylist = ['apple', 'banana', 'cherry']
-mylist.insert(0, 'orange')
-print(mylist[1])
-fruits = ["apple", "banana", "cherry"]
-fruits.insert(1, "orange")
-fruits = ['apple', 'banana', 'cherry']
-tropical = ['mango', 'pineapple', 'papaya']
-fruits.extend(tropical)
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
