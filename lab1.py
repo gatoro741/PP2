1e681ad68f52970a2ce9fa48102d7992426cdfd0
gatoro741 <olegatorgator@gmail.com>
2025-01-24 20:06:32 +0500

    exercise add list items

--- lab1.py
+++ lab1.py
@@ -1,4 +1,8 @@
-thislist = ["apple", "banana", "cherry"]
-tropical = ["mango", "pineapple", "papaya"]
-thislist.extend(tropical)
-print(thislist)
+mylist = ['apple', 'banana', 'cherry']
+mylist.insert(0, 'orange')
+print(mylist[1])
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "orange")
+fruits = ['apple', 'banana', 'cherry']
+tropical = ['mango', 'pineapple', 'papaya']
+fruits.extend(tropical)
