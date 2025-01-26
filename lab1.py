eed2de30a32deac42369a1e410d927cfaf79c777
gatoro741 <olegatorgator@gmail.com>
2025-01-26 23:30:10 +0500

    check if key exist

--- lab1.py
+++ lab1.py
@@ -1,13 +1,7 @@
-car = {
-"brand": "Ford",
-"model": "Mustang",
-"year": 1964
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
 }
-
-x = car.values()
-
-print(x) #before the change
-
-car["year"] = 2020
-
-print(x) #after the change
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
