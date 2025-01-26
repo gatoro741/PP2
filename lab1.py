5650c54837eece6cfac4d27acf3999a031b90383
gatoro741 <olegatorgator@gmail.com>
2025-01-26 20:28:25 +0500

    reverse sort

--- lab1.py
+++ lab1.py
@@ -1,3 +1,6 @@
 thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
-thislist.sort()
+thislist.sort(reverse = True)
 print(thislist)
+thislist2 = [100, 50, 65, 82, 23]
+thislist2.sort(reverse = True)
+print(thislist2)
