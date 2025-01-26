0d4f2e9588389b3db1debf203ffae53c86dde339
gatoro741 <olegatorgator@gmail.com>
2025-01-26 22:11:49 +0500

    change tuple values

--- lab1.py
+++ lab1.py
@@ -1,3 +1,6 @@
-thistuple = ("apple", "banana", "cherry")
-if "apple" in thistuple:
-  print("Yes, 'apple' is in the fruits tuple")
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
