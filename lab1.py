8ade22023e0a5a18564ce361ae4d74b6025cd63b
gatoro741 <olegatorgator@gmail.com>
2025-01-26 23:18:21 +0500

    loop items

--- lab1.py
+++ lab1.py
@@ -1,19 +1,4 @@
 thisset = {"apple", "banana", "cherry"}
 
-x = thisset.pop()
-
-print(x)
-
-print(thisset)
-
-thisset = {"apple", "banana", "cherry"}
-
-thisset.clear()
-
-print(thisset)
-
-thisset = {"apple", "banana", "cherry"}
-
-del thisset
-
-print(thisset)
+for x in thisset:
+  print(x)
