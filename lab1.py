ab442b9d5ec58d969cac275986e2a9c0c3293def
gatoro741 <olegatorgator@gmail.com>
2025-01-26 23:44:07 +0500

    and

--- lab1.py
+++ lab1.py
@@ -1,8 +1,5 @@
 a = 200
 b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
-else:
-  print("a is greater than b")
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
