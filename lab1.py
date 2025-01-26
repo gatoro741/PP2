95867c4b13aa3373ce7844bdac25f486181f0ab7
gatoro741 <olegatorgator@gmail.com>
2025-01-26 23:41:00 +0500

    Nested Dictionaries

--- lab1.py
+++ lab1.py
@@ -1,7 +1,14 @@
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
 }
-mydict = thisdict.copy()
-print(mydict)
