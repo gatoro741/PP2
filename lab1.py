da014abc3e6cd21b3fcfe0a562d4ed183222505c
gatoro741 <olegatorgator@gmail.com>
2025-01-26 20:51:45 +0500

    Case Insensitive Sort

--- lab1.py
+++ lab1.py
@@ -1,6 +1,3 @@
-def myfunc(n):
-  return abs(n - 50)
-
-thislist = [100, 50, 65, 82, 23]
-thislist.sort(key = myfunc)
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
 print(thislist)
