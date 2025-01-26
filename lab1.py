959c97b47c65ae6f1f4a905989df0b047a2ab952
gatoro741 <olegatorgator@gmail.com>
2025-01-26 22:21:37 +0500

    join two tuples

--- lab1.py
+++ lab1.py
@@ -1,5 +1,5 @@
-thistuple = ("apple", "banana", "cherry")
-i = 0
-while i < len(thistuple):
-  print(thistuple[i])
-  i = i + 1
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
