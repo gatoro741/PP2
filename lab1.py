b57ec8c46a45326f39e7e1e75200ca8b67f7f652
gatoro741 <olegatorgator@gmail.com>
2025-01-26 23:23:02 +0500

    intersection

--- lab1.py
+++ lab1.py
@@ -1,5 +1,5 @@
-set1 = {"a", "b" , "c"}
-set2 = {1, 2, 3}
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
 
-set1.update(set2)
-print(set1)
+set3 = set1.intersection(set2)
+print(set3)
