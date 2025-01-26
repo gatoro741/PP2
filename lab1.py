695a7e35db0f9c1c42255ee2bc7b580e68b23e49
gatoro741 <olegatorgator@gmail.com>
2025-01-26 23:44:51 +0500

    nested if

--- lab1.py
+++ lab1.py
@@ -1,4 +1,8 @@
-a = 33
-b = 200
-if not a > b:
-  print("a is NOT greater than b")
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
