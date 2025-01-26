a0e2ac4f5232d00558e503e622d069eb9f5e940d
gatoro741 <olegatorgator@gmail.com>
2025-01-26 23:20:41 +0500

    join multiple sets

--- lab1.py
+++ lab1.py
@@ -1,5 +1,7 @@
 set1 = {"a", "b", "c"}
 set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
 
-set3 = set1.union(set2)
-print(set3)
+myset = set1.union(set2, set3, set4)
+print(myset)
