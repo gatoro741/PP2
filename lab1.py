5e64f9e1587ef4151873bc22b4a35a142441d9d9
gatoro741 <olegatorgator@gmail.com>
2025-01-26 20:21:20 +0500

    expression

--- lab1.py
+++ lab1.py
@@ -1,8 +1,1 @@
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = []
-
-for x in fruits:
-  if "a" in x:
-    newlist.append(x)
-
-print(newlist)
+newlist = [x.upper() for x in fruits]
