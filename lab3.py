5cfb7f26c4b2e89d3de5a1aeb0a4d7a87d64a40f
gatoro741 <olegatorgator@gmail.com>
2025-01-28 14:29:01 +0500

    add methods

--- lab3.py
+++ lab3.py
@@ -12,3 +12,7 @@ x.printname()
 class Student(Person):
   def __init__(self, fname, lname):
     super().__init__(fname, lname)
+    self.graduationyear = year
+    
+def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
