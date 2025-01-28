05678c725e388d066b6c71d4d88a54f33f799bbf
gatoro741 <olegatorgator@gmail.com>
2025-01-28 13:46:47 +0500

    keyword arguments

--- lab3.py
+++ lab3.py
@@ -1,4 +1,4 @@
-def my_function(*kids):
-  print("The youngest child is " + kids[2])
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
 
-my_function("Emil", "Tobias", "Linus")
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
