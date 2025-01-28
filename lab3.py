052ab64fb74ba10b78f89e47d83c9a8fdcd75e37
gatoro741 <olegatorgator@gmail.com>
2025-01-28 14:22:46 +0500

    __init__ func

--- lab3.py
+++ lab3.py
@@ -1,5 +1,9 @@
-class MyClass:
-  x = 5
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
-p1 = MyClass()
-print(p1.x)
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
