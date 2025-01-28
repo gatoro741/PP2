2435825dce4c31fedbbac4b134ccb0c347b26378
gatoro741 <olegatorgator@gmail.com>
2025-01-28 13:47:33 +0500

    **kwargs

--- lab3.py
+++ lab3.py
@@ -1,4 +1,4 @@
-def my_function(child3, child2, child1):
-  print("The youngest child is " + child3)
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
 
-my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+my_function(fname = "Tobias", lname = "Refsnes")
