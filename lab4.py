e4d1910a2947863fea7d692b03edc196d8a9746f
gatoro741 <olegatorgator@gmail.com>
2025-02-11 13:51:16 +0500

    drop microseconds from datetime.

--- lab4.py
+++ lab4.py
@@ -1,6 +1,5 @@
 import datetime
 
 current_date = datetime.datetime.today()
-yesterday_date = current_date - datetime.timedelta(days=1)
-tommorow_date = current_date + datetime.timedelta(days=1)
-print(yesterday_date.strftime('%Y-%m-%d') , current_date.strftime('%Y-%m-%d') , tommorow_date.strftime('%Y-%m-%d'))
+
+print(current_date.strftime('%Y-%m-%d-%X'))
