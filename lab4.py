73ea9c4c6616b1302077df94071ebaf60c3fc9fa
gatoro741 <olegatorgator@gmail.com>
2025-02-14 16:40:46 +0500

    convert degree to radian

--- lab4.py
+++ lab4.py
@@ -1,5 +1,3 @@
-import datetime
-
-current_date = datetime.datetime.today()
-yesterday_date = current_date - datetime.timedelta(days=1)
-print(abs((current_date - yesterday_date).total_seconds()))
+import math 
+degree = input()
+print((float(degree)*math.pi)/180)
