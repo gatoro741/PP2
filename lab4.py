f1bd9f342eb41b32bc9c716cf873fef6291d23e4
gatoro741 <olegatorgator@gmail.com>
2025-02-14 16:49:13 +0500

    area of trapezoid

--- lab4.py
+++ lab4.py
@@ -1,3 +1,6 @@
 import math 
-degree = input()
-print((float(degree)*math.pi)/180)
+height = int(input("Height: "))
+base1 = int(input("Base, fist value: "))
+base2 = int(input("Base, second value: "))
+answer = ((base1 + base2) * height)/2
+print("Expected Output: ", answer)
