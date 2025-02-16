b96660df033bfe9069435c25f4f51fa481a042f0
gatoro741 <olegatorgator@gmail.com>
2025-02-16 23:30:38 +0500

    generator which can iterate the numbers, which are divisible by 3 and 4, between a given range 0 and n.

--- lab4.py
+++ lab4.py
@@ -1,6 +1,8 @@
-def even_numbers(n):
-    for i in range(0, n + 1, 2):
-        yield str(i)  
+def divisible_by_3_and_4(n):
+    for i in range(n + 1):  
+        if i % 3 == 0 and i % 4 == 0: 
+            yield i  
 
 n = int(input("Enter a number: "))
-print(", ".join(even_numbers(n))) 
+for num in divisible_by_3_and_4(n):
+    print(num)
