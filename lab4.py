109dd2b2d50bc97aa830be3130a5625bb46c44e3
gatoro741 <olegatorgator@gmail.com>
2025-02-16 23:17:57 +0500

    generator that generates the squares

--- lab4.py
+++ lab4.py
@@ -1,5 +1,7 @@
-import math
-base = float(input("Length of base: "))  
-height = float(input("Height of parallelogram: "))  
-parallelogram_area = base * height  
-print("Expected Output:", parallelogram_area)  
+def square_generator(N):
+    for i in range(N + 1):
+        yield i ** 2  
+
+N = int(input("Enter a number: "))
+for square in square_generator(N):
+    print(square)
